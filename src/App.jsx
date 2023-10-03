50f60f6fb23f49331f671ac3674a4c47738c16f2
v1m4 <20565102-v1m4@users.noreply.replit.com>
2023-10-03 12:01:28 +0000

    tela de login sem firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,55 @@
 // src/App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import OrderForm from './components/OrderForm';
 import OrderList from './components/OrderList';
+import Login from './components/Login';
 
 import logo from './assets/logo.png';
-import './App.css'; // Importe o arquivo CSS
+import './App.css';
 
 const App = () => {
-  const [orders, setOrders] = useState([]);
+  const [user, setUser] = useState(null);
+  const [orders, setOrders] = useState(() => {
+    const savedOrders = JSON.parse(localStorage.getItem('orders'));
+    return savedOrders || [];
+  });
+
+  const handleLogin = ({ username, password }) => {
+    // Simulação simples de login - normalmente você faria uma verificação mais complexa
+    if (username === 'admin' && password === 'password') {
+      setUser(username);
+    } else {
+      alert('Credenciais inválidas');
+    }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
 
   const addOrder = (newOrder) => {
     setOrders([...orders, newOrder]);
   };
 
+  useEffect(() => {
+    localStorage.setItem('orders', JSON.stringify(orders));
+  }, [orders]);
+
   return (
     <div>
-      <header className="app-header"> {/* Adicione a classe "app-header" */}
-        <img src={logo} alt="Logo da Empresa" className="app-logo" /> {/* Adicione a classe "app-logo" */}
-        <h1>Sistema de Ordens de Serviço</h1>
-      </header>
-      <OrderForm addOrder={addOrder} />
-      <OrderList orders={orders} />
+      {user ? (
+        <div>
+          <header className="app-header">
+            <img src={logo} alt="Logo da Empresa" className="app-logo" />
+            <h1>Sistema de Ordens de Serviço</h1>
+            <button onClick={handleLogout}>Logout</button>
+          </header>
+          <OrderForm addOrder={addOrder} />
+          <OrderList orders={orders} />
+        </div>
+      ) : (
+        <Login handleLogin={handleLogin} />
+      )}
     </div>
   );
 };
---
+++ src/components/Login.jsx
@@ -0,0 +1,41 @@
+// src/components/Login.js
+import React, { useState } from 'react';
+
+const Login = ({ handleLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleLogin({ username, password });
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Usuário:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
